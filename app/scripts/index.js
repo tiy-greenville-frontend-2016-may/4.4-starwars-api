57fe4678f41df88330257e2264ba0fb81d61b3f8
Dan Dietz <danddietz@gmail.com>
2016-06-09 12:03:16 -0400

    first ajax request.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,32 @@
-console.log("Hello World!");
+var $ = require('jquery');
+var handlebars = require('handlebars');
+
+
+var baseUrl = 'http://swapi.co/api/';
+var planetListItemTemplate = $('.planet-list-item-template').html();
+var template = handlebars.compile(planetListItemTemplate);
+
+$('.js-planets-button').on('click', function(event){
+  event.preventDefault();
+
+  getPlanets();
+});
+
+function getPlanets(){
+  var planetsUrl = baseUrl + 'planets/';
+
+  $.ajax(planetsUrl).done(function(planetList){
+    planetList.results.forEach(function(planet){
+      displayPlanet(planet);
+    });
+  });
+}
+
+function displayPlanet(planet){
+  var html = template(planet)
+  $('.js-planet-list').append(html);
+
+  // $.ajax().done(function(data){
+  //   $('#' + )
+  // });
+}
